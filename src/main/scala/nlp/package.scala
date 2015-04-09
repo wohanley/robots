ac5a58eb67629f61653129922070198862b54adf
William O'Hanley <liam@wohanley.com>
2015-04-09 19:32:27 -0300

    change package prefixes

--- src/main/scala/nlp/package.scala
+++
@@ -1,6 +0,0 @@
-/** Provides some tools for dealing with natural language.
-  * 
-  * This package is built on Apache OpenNLP and intended as a complement
-  * thereto. For example, nlp.parsing does not actually provide a way to
-  * instantiate parsers - do it the OpenNLP way. */
-package object nlp {}
