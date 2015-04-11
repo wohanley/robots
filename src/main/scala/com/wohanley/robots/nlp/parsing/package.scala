aa345f42ae5167139545d14152dc21f8f12c9abb
William O'Hanley <liam@wohanley.com>
2015-04-11 12:04:08 -0300

    reorganize some parsing stuff

--- src/main/scala/com/wohanley/robots/nlp/parsing/package.scala
+++ src/main/scala/com/wohanley/robots/nlp/parsing/package.scala
@@ -7,8 +7,10 @@ package object parsing {
   import java.io.FileInputStream
   import opennlp.tools.parser.AbstractBottomUpParser
   import opennlp.tools.parser.Parse
+  import opennlp.tools.parser.Parser
   import opennlp.tools.parser.ParserFactory
   import opennlp.tools.parser.ParserModel
+  import opennlp.tools.tokenize.Tokenizer
   import opennlp.tools.util.Span
 
 
@@ -16,26 +18,29 @@ package object parsing {
     new ParserModel(
       new FileInputStream("en-parser-chunking.bin")))
 
-  /* Lifted from http://blog.dpdearing.com/2011/12/
-   * how-to-use-the-opennlp-1-5-0-parser/ */
-  def parse(text: String): Parse = {
+  /** Returns a parse tree for the given text. */
+  def parse(tokenizer: Tokenizer)(parser: Parser)(text: String): Parse = {
+    /* Lifted from http://blog.dpdearing.com/2011/12/
+     * how-to-use-the-opennlp-1-5-0-parser/ */
+    val spans = tokenizer.tokenizePos(text);
+
     val p = new Parse(text,
       new Span(0, text.length()), AbstractBottomUpParser.INC_NODE, 1, 0)
-
-    val spans = englishTokenizer.tokenizePos(text);
 
     spans.zipWithIndex.foreach { case (span, index) =>
       // flesh out the parse with individual token sub-parses 
       p.insert(new Parse(text, span, AbstractBottomUpParser.TOK_NODE, 0, index))
     }
 
-    englishParser.parse(p);
+    parser.parse(p);
   }
 
   /** Search through a parse tree for nodes that satisfy a predicate. */
   def filter(f: Parse => Boolean)(found: Seq[Parse])(node: Parse):
       Seq[Parse] = {
-
+    /* I'd like to revisit this and try to find a way to wrap an OpenNLP parse
+     * tree with an actual tree class, maybe Scalaz's Tree, but I can't figure
+     * out how Scalaz's Tree works right now at all */
     val foundAfterNode: Seq[Parse] = if (f(node)) {
       found :+ node
     } else {
